46acaaa8d97ec8d120ca2a8048978387c7745741
Yastremskiy <oleg.yastremskiy@chatbots.studio>
2023-02-15 11:14:32 +0200

    Added documentation to class

--- index.ts
+++ index.ts
@@ -3,16 +3,29 @@ const task = async <T>(value: T) => {
   console.log(value);
 };
 
+/**
+ * Class for working with async queues
+ */
 class AsyncQueue {
   private tasks: Function[] = [];
   private isPending = false;
 
+  /**
+   * Method for adding task to queue
+   * @param fn - task - that needs to be added and handled in queue
+   * @return - nothing needs to be returned
+   */
   async add(fn: Function): Promise<void> {
     this.tasks.push(fn);
     await this.work();
     return;
   }
 
+  /**
+   * Method for working with tasks in queue
+   * @params - empty
+   * @return - nothing needs to be returned
+   */
   async work(): Promise<void> {
     if (!this.tasks.length || this.isPending) return;
     this.isPending = true;
